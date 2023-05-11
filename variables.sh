fa5e53549b0416a9e1059210e2518abcc4815d0d
Brother-man <85408312+Serene7n@users.noreply.github.com>
2023-05-11 11:06:25 +0100

    Update variables.sh

--- variables.sh
+++ variables.sh
@@ -2,10 +2,10 @@ GIT_COMMIT=$(get rev-parse master)
 aws_account='679573946489.dkr.ecr.ap-south-1.amazonaws.com'
 ecrImageName=$aws_account"/veros-intergrated-foresight"
 
-docker stop $(docker ps -aq) && docker rm $(docker ps -aq)
-docker system prune -af  && docker volume rm $(docker volume ls --filter dangling=true -q)
+docker ps -aq | docker stop && docker ps -aq | docker rm 
+docker system prune -af  && docker volume ls --filter dangling=true -q | docker volume rm 
 
-docker build -t $ecrImageName:latest $ecrImageName:$GIT_COMMIT
+docker build -t $ecrImageName:latest $ecrImageName:$GIT_COMMIT .
 
 aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 679573946489.dkr.ecr.ap-south-1.amazonaws.com
 
